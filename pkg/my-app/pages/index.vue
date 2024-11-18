cd0c6b0332404b9310cf8220de4ae890fcc2d394
Yi-Ya Chen <yiya.chen@suse.com>
2024-11-18 11:44:14 +0800

    feat: introduce veaury

--- pkg/my-app/pages/index.vue
+++ pkg/my-app/pages/index.vue
@@ -1,14 +1,29 @@
 <script>
-export default { name: 'IndexPage' };
+import {applyReactInVue, applyPureReactInVue} from 'veaury'
+import BasicReactComponent from './react_app/Basic.jsx'
+import {ref} from 'vue'
+
+export default {
+  components: {
+    Basic: applyReactInVue(BasicReactComponent),
+    BasicPure: applyPureReactInVue(BasicReactComponent)
+  },
+  setup() {
+    return {
+      foo: ref('Hello!')
+    }
+  }
+}
 </script>
 
 <template>
+  <BasicPure :foo="foo">
+    <div>
+      the children
+    </div>
+  </BasicPure>
   <div>
     <h1>Top Level Product</h1>
     <p>This is a custom VueJS page. You can render anything you want</p>
   </div>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,23 @@
 const config = require('@rancher/shell/vue.config'); // eslint-disable-line @typescript-eslint/no-var-requires
 
-module.exports = config(__dirname, {
+const baseConfig = config(__dirname, {
   excludes: [],
-  // excludes: ['fleet', 'example']
 });
+
+// add custom loader
+if (typeof baseConfig.configureWebpack === 'function') {
+  const configureWebpack = baseConfig.configureWebpack;
+
+  baseConfig.configureWebpack = (config) => {
+    configureWebpack(config);
+
+    config.module.rules.push({
+      test: /\.jsx$/i,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: { presets: ['veaury/babel/ReactPreset'] }
+    });
+  };
+}
+
+module.exports = baseConfig;
