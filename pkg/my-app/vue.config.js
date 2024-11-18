60d9ab4b5ba44cc723255d84f532f52e36ebeedc
Yi-Ya Chen <yiya.chen@suse.com>
2024-11-18 15:38:57 +0800

    chore: fix production build error

--- pkg/my-app/vue.config.js
+++ pkg/my-app/vue.config.js
@@ -1,1 +1,19 @@
-module.exports = require('./.shell/pkg/vue.config')(__dirname);
+const baseConfig = require('./.shell/pkg/vue.config')(__dirname);
+
+// add custom loader
+if (typeof baseConfig.configureWebpack === 'function') {
+  const configureWebpack = baseConfig.configureWebpack;
+
+  baseConfig.configureWebpack = (config) => {
+    configureWebpack(config);
+
+    config.module.rules.push({
+      test: /\.jsx$/i,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: { presets: ['veaury/babel/ReactPreset'] }
+    });
+  };
+}
+
+module.exports = baseConfig;
