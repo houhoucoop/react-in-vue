bec1d866edc1d1b4ba0d562ae991dd516539b5d1
Yi-Ya Chen <yiya.chen@suse.com>
2024-11-20 15:03:45 +0800

    feat: add bundle config

--- pkg/my-app-2/vue.config.js
+++ pkg/my-app-2/vue.config.js
@@ -1,1 +1,36 @@
-module.exports = require('./.shell/pkg/vue.config')(__dirname);
+const baseConfig = require('./.shell/pkg/vue.config')(__dirname);
+const webpack = require('webpack');
+
+const remoteURL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
+if (typeof baseConfig.configureWebpack === 'function') {
+  const configureWebpack = baseConfig.configureWebpack;
+
+  baseConfig.configureWebpack = (webpackConfig) => {
+    configureWebpack(webpackConfig);
+
+    webpackConfig.plugins.push(
+      new webpack.container.ModuleFederationPlugin({
+        name: 'longhorn',
+        remotes: {
+          longhornUI: `longhornUI@${remoteURL}/remoteEntry.js`
+        }
+      }),
+    )
+  };
+}
+
+baseConfig.devServer = {
+  ...baseConfig.devServer,
+  proxy: {
+    '/longhorn/c/_/v1': {
+      target: remoteURL,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/longhorn/c/_/v1': '/v1',
+      },
+    },
+  },
+}
+
+module.exports = baseConfig
